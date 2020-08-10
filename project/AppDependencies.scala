01522e422cc412ed36c81f497b05b0731bc1b5fd
Nigel Perkins <nigelhp@users.noreply.github.com>
2020-08-10 09:14:42 +0100

    BDOG-994: uplift simple-reactivemongo and other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-play-26"    % "1.6.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.26.0-play-26",
+    "uk.gov.hmrc" %% "bootstrap-play-26"    % "1.14.0",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.30.0-play-26",
     ws
   )
 
@@ -14,7 +14,7 @@ object AppDependencies {
     "org.scalatest"     %% "scalatest"          % "3.0.8"             % Test,
     "org.pegdown"       % "pegdown"             % "1.6.0"             % Test,
     "com.typesafe.play" %% "play-test"          % PlayVersion.current % Test,
-    "uk.gov.hmrc"       %% "reactivemongo-test" % "4.19.0-play-26"    % Test
+    "uk.gov.hmrc"       %% "reactivemongo-test" % "4.21.0-play-26"    % Test
   )
 
 }
